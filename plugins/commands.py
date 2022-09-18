9fdb25bdfd01754238e27d66d03bcd0e2eca20c2
AKR <98525798+Akrtechi@users.noreply.github.com>
2022-09-18 12:51:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                 ],[
                     InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES')
-                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA')
+                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA'),
                 ],[
                     InlineKeyboardButton('HELP ⚠️', callback_data='help'),
                     InlineKeyboardButton('ABOUT ⚙', callback_data='about')
@@ -96,7 +96,7 @@ async def start(client, message):
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                 ],[
                     InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES')
-                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA')
+                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA'),
                 ],[
                     InlineKeyboardButton('HELP ⚠️', callback_data='help'),
                     InlineKeyboardButton('ABOUT ⚙', callback_data='about')

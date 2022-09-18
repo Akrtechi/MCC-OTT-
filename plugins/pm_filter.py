f6751dca37367e5f9d0d2f4335272c716f357aa2
AKR <98525798+Akrtechi@users.noreply.github.com>
2022-09-18 09:29:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -458,7 +458,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                 ],[
-                    InlineKeyboardButton('MY GROUP 📌', utl='https://t.me/MM_ARCHIVES')
+                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES')
                     InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA')
                 ],[
                     InlineKeyboardButton('HELP ⚠️', callback_data='help'),

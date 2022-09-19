ba1554bcad688a236948c8f3b5669c40eaf87456
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-09-19 18:51:50 +0530

    Error Fixed ✅
    Button error fixed ✅

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,8 +24,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                 ],[
-                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES')
-                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA'),
+                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES'),
+                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA')
                 ],[
                     InlineKeyboardButton('HELP ⚠️', callback_data='help'),
                     InlineKeyboardButton('ABOUT ⚙', callback_data='about')
@@ -47,8 +47,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                 ],[
-                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES')
-                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA'),
+                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES'),
+                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA')
                 ],[
                     InlineKeyboardButton('HELP ⚠️', callback_data='help'),
                     InlineKeyboardButton('ABOUT ⚙', callback_data='about')
@@ -95,8 +95,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'https://t.me/auto_m4_mallumovies_bot?startgroup=true')
                 ],[
-                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES')
-                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA'),
+                    InlineKeyboardButton('MY GROUP 📌', url='https://t.me/MM_ARCHIVES'),
+                    InlineKeyboardButton('MY OWNER ⚒', url='https://t.me/MOVIE_MEADIA')
                 ],[
                     InlineKeyboardButton('HELP ⚠️', callback_data='help'),
                     InlineKeyboardButton('ABOUT ⚙', callback_data='about')
